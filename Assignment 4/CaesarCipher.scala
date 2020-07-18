e394f7afc274ce2aed5c9d0b38cd00833dae2c18
NirushiWijesiri <56571293+NirushiWijesiri@users.noreply.github.com>
2020-07-18 16:58:33 +0530

    Add files via upload

---
+++ Assignment 4/CaesarCipher.scala
@@ -0,0 +1,20 @@
+import scala.io.StdIn.readInt
+import scala.io.StdIn.readLine
+object CaesarCipher extends App{
+
+  val Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  val Encryption=(c:Char, key:Int, a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
+  val Decryption=(c:Char, key:Int, a:String)=>a((a.indexOf(c.toUpper)-key)%a.size)
+  val Cipher=(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
+
+  def Result():Unit={ 
+    printf("Enter a text : ")
+    val s = readLine()
+    printf("Enter a shift number : ")
+    val key = readInt()
+    val ct = Cipher(Encryption,s,key,Alphabet)
+    val pt = Cipher(Decryption,ct,key,Alphabet)
+    printf("The Encrypted text : %s \nThe Decrypted text : %s",ct,pt)
+  }
+  Result()
+}
